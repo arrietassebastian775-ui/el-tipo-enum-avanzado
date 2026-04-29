1fa4e0a1e00b252d6e4eb6a95d91e0d9580dcd0f
arrietassebastian775-ui <arrietassebastian775@gmail.com>
2026-04-29 14:31:26 +0200

    Lo basico del enum explicado

--- el_tipo_enum_avanzado/src/main/java/com/example/App.java
+++ el_tipo_enum_avanzado/src/main/java/com/example/App.java
@@ -2,6 +2,17 @@ package com.example;
 
 public class App {
     public static void main(String[] args) {
+    	
+    	/*El enum tiene un metodo que se llama values, que permite iterar a travez de las constantes del enum
+    	 * y mostrar el ordinal asiciado a dicha constante el nombre de tipo String de la propia constante*/
+    	
+    	// primero utilizando sentencia for mejorado
+    	
+    	for (Season season : Season.values()) {
+    		System.out.println("Nombre de la constante: " + season.name() + ", ordinal: " + season.ordinal());
+    	}
+    	
+    	// segundo utilizando operaciones de agregado
 
     }
 }
